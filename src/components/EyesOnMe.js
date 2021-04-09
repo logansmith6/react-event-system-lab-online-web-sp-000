12a246374ce4d79cedc73ccb26072337f25d2fd6
Logan Smith <logsmith06@gmail.com>
2021-04-09 17:54:16 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 
 export default class EyesOnMe extends Component {
 
+<<<<<<< HEAD
 blur = () => {
     console.log('Hey! Eyes on me!')
 }
@@ -14,6 +15,11 @@ focus = () => {
   render(){
     return(
       <button onFocus={this.focus} onBlur={this.blur}></button>
+=======
+  render(){
+    return(
+      <button onFocus={console.log('Good!')} onBlur={console.log('Hey! Eyes on me!')}></button
+>>>>>>> 080d7d663abe275218dab29e812576e6007e397a
     )
   }
 
